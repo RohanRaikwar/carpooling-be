50d9bf36b25c1f71e371ed265167c427a4a69127
Saurabh-dev15 <saurabh.singh@bttnusa.com>
2026-01-30 13:14:48 +0530

    pm 2

---
+++ ecosystem.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  apps: [
+    {
+      name: 'api-server',
+      script: 'dist/server.js',
+      instances: 1, // increase if needed
+      exec_mode: 'fork', // cluster if you want scaling
+      env: {
+        NODE_ENV: 'production',
+      },
+      error_file: 'logs/api-error.log',
+      out_file: 'logs/api-out.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+      autorestart: true,
+      max_restarts: 10,
+    },
+    {
+      name: 'mail-worker',
+      script: 'dist/modules/mail/mail.worker.js',
+      instances: 1,
+      exec_mode: 'fork',
+      env: {
+        NODE_ENV: 'production',
+      },
+      error_file: 'logs/worker-error.log',
+      out_file: 'logs/worker-out.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+      autorestart: true,
+      max_restarts: 10,
+    },
+  ],
+};
